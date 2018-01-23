15c36bd51e7b2170d2aa408d20a561313e3f73c3
Szymon Janota <szymon.janota@relayr.io>
2018-01-23 21:21:02 +0100

    use PORT env variable to start server

--- src/main/scala/com/github/sursmobil/Main.scala
+++ src/main/scala/com/github/sursmobil/Main.scala
@@ -5,19 +5,21 @@ import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
 import com.typesafe.scalalogging.StrictLogging
 import com.github.sursmobil.http.Routes
+import com.typesafe.config.{Config, ConfigFactory}
 
 import scala.concurrent.{ExecutionContext, ExecutionContextExecutor, Future}
 
 object Main {
+  val config: Config = ConfigFactory.load()
   // needed to run the route
-  implicit val system: ActorSystem = ActorSystem()
+  implicit val system: ActorSystem = ActorSystem("cookhub", config)
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   // needed for the future map/flatmap in the end and future in fetchItem and saveOrder
   implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
   def main(args: Array[String]): Unit = {
 
-    val bindingFuture = Http().bindAndHandle(Routes, "localhost", 8080)
+    val bindingFuture = Http().bindAndHandle(Routes, "localhost", config.getInt("cookhub.server.port"))
 
     sys.addShutdownHook {
       bindingFuture
